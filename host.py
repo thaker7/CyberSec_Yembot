4cab2141274ce25bd98e0fe11bd820653a493c70
Omer_Bawazir <omer.kh.bawazeer@gmail.com>
2024-01-22 00:41:29 +0400

    Create requirements.txt

--- host.py
+++ host.py
@@ -5,10 +5,10 @@ app = Flask('')
 
 @app.route('/')
 def home():
-    return "<b> hello Cyber Security</b>"
+    return "<b> hell Cyber Security</b>"
 
 def run():
-    app.run(host='0.0.0.0', port=8080)
+    app.run(host='0.0.0.0', port=3001)
 
 def keep_alive():
     t = Thread(target=run)
