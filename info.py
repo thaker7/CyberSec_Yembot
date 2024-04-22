9a0c916a2893b83d4f3c00c5d75cf2b87b75ba74
thaker7 <166921231+thaker7@users.noreply.github.com>
2024-04-22 10:34:21 +0300

    Update info.py

--- info.py
+++ info.py
@@ -1,5 +1,5 @@
 import telebot
 import time
 from telebot import types
-TOKEN_BOT = "6758499492:AAG-E9c1uDgXwAMgXGVH-aWpUDjfYG-spvw"
+TOKEN_BOT = "6906656950:AAFf1XiZ_SXBl1A1H7NMAHVEU9W9v1-Ag5M"
 bot = telebot.TeleBot(TOKEN_BOT)
